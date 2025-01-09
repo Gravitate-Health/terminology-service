100328ec8766e2b00ea8034c9b541a9899028eaf
Alejandro Alonso López <aalonsopersonal@gmail.com>
2025-01-09 17:28:47 +0100

    Now using Papaparse and changed the code

--- controllers/codes-controller.js
+++ controllers/codes-controller.js
@@ -1,53 +1,35 @@
-const csv = require('csvtojson');
+const papa = require('papaparse');
 
 async function getAll(req,res) {
-  let pregnancyList;
+  let codesList = [];
 
-  let csvPregFile = await fetch("https://raw.githubusercontent.com/Gravitate-Health/terminology-service/main/controllers/db/codes.csv")
-    .then(response => response.text())
-    .then(text => text);
+  let codesResponse = await fetch("https://raw.githubusercontent.com/Gravitate-Health/terminology-service/main/controllers/db/codes.csv")
 
-  let csvSynonymsFile = await fetch("https://raw.githubusercontent.com/Gravitate-Health/terminology-service/main/controllers/db/synonyms.csv")
-    .then(response => response.text())
-    .then(text => text);
+  let csvCodesFile = await codesResponse.text();
 
-  csv().fromString(csvPregFile).then((list)=>{
-    console.log(list)
-    pregnancyList = list;
+  let synonymResponse = await fetch("https://raw.githubusercontent.com/Gravitate-Health/terminology-service/main/controllers/db/synonyms.csv")
+
+  let csvSynonymsFile = await synonymResponse.text();
+
+  codesList = papa.parse(csvCodesFile, {header: true}).data;
+
+  // 
+  
+  let synonymsList = papa.parse(csvSynonymsFile, {header: true}).data;
+
+  let result = codesList.map((term) => {
+    let synonyms = synonymsList.filter((synonym) => synonym.source_system === term.codesystem && synonym.source_code === term.code);
+
+    if (synonyms.length === 0) {
+      return term;
+    }
+
+    return {
+      ...term,
+      synonyms
+    }
   });
-
-  csv().fromString(csvSynonymsFile).then((list)=>{
-    console.log(list)
-    let result = [];
-    result = pregnancyList;
-    pregnancyList.forEach((pregnancyCode) => {
-      let synonyms = list.filter((synonym) => {
-        return (synonym.source_system == pregnancyCode.codesystem && synonym.source_code == pregnancyCode.code);
-      });
-      
-      if(synonyms.length > 0) {
-        synonyms.forEach((synonym) => {
-          linkedTerm = pregnancyList.filter((pregnancyCode) => {
-            return (pregnancyCode.codesystem == synonym.source_system && pregnancyCode.code == synonym.source_code);
-          })
-          result.push({
-            codesystem: synonym.target_system,
-            code: synonym.target_code,
-            descr_en: synonym.target_description_en,
-            descr_es: synonym.target_description_es,
-            synonym_of: {
-              codesystem: synonym.source_system,
-              code: synonym.source_code,
-              descr_en: linkedTerm[0].descr_en,
-              descr_es: linkedTerm[0].descr_es
-            }
-          });
-        });
-      }
-    });
-
     res.status(200).json(result);
-  });
 }
 module.exports = {
   getAll
